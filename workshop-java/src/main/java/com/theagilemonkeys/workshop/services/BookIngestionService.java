73ad8d85cc75b6c92940caaed071b27164d42d51
BorjaZarco <borjazarco@theagilemonkeys.com>
2023-08-03 10:53:43 +0100

    Improve BookIngestionService

--- workshop-java/src/main/java/com/theagilemonkeys/workshop/services/BookIngestionService.java
+++ workshop-java/src/main/java/com/theagilemonkeys/workshop/services/BookIngestionService.java
@@ -1,28 +1,27 @@
 package com.theagilemonkeys.workshop.services;
 
+import org.springframework.stereotype.Service;
+
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class BookIngestionService {
 
-    private final String file_path;
-    public BookIngestionService (String file_path) {
-        this.file_path = file_path;
-    }
-
     /**
-     * This function reads a book (file) and splits its content into chunks of 1000 characters or less.
+     * This function reads a book (file) and splits its content into chunks of 1000 characters or fewer.
      * Each chunk is added to a list which is then returned as output.
      * If the book (file) cannot be read due to an IOException, the stack trace of the exception is printed and an empty list is returned.
      */
-    public List<String> ProcessBookByCharacters() {
+    public static List<String> processBookByCharacters(String filePath) {
         List<String> chunks = new ArrayList<>();
 
         try {
-            String content = new String(Files.readAllBytes(new File(this.file_path).toPath()));
+            File file = new File(filePath);
+            String content = new String(Files.readAllBytes(file.toPath()));
             int index = 0;
             while (index < content.length()) {
                 chunks.add(content.substring(index, Math.min(index + 1000, content.length())));

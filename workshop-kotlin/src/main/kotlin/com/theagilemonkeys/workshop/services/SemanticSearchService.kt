4bc609e0082b3d45858bec6f01ae256095a75b50
Adrián Lorenzo Melián <adrian@theagilemonkeys.com>
2023-08-03 12:43:24 +0100

    Remove SemanticSearch initialization code

--- workshop-kotlin/src/main/kotlin/com/theagilemonkeys/workshop/services/SemanticSearchService.kt
+++ workshop-kotlin/src/main/kotlin/com/theagilemonkeys/workshop/services/SemanticSearchService.kt
@@ -1,30 +1,16 @@
 package com.theagilemonkeys.workshop.services
 
-import com.aallam.openai.client.OpenAI
-import com.theagilemonkeys.ellmental.embeddingsmodel.openai.OpenAIEmbeddingsModel
 import com.theagilemonkeys.ellmental.semanticsearch.LearnInput
 import com.theagilemonkeys.ellmental.semanticsearch.SearchOutput
 import com.theagilemonkeys.ellmental.semanticsearch.SemanticSearch
 import com.theagilemonkeys.workshop.config.SemanticSearchConfiguration
-import com.theagilemonkeys.ellmental.vectorstore.pinecone.PineconeVectorStore
 import org.springframework.stereotype.Service
 
 
 @Service
 class SemanticSearchService(private val semanticSearchConfiguration: SemanticSearchConfiguration) {
     private val semanticSearch: SemanticSearch = run {
-        val openAIKey = semanticSearchConfiguration.openaiKey
-        val pineconeKey = semanticSearchConfiguration.pineconeKey
-        val pineconeUrl = semanticSearchConfiguration.pineconeUrl
-        val pineconeNamespace = semanticSearchConfiguration.pineconeNamespace
-
-        with(OpenAI(token = openAIKey)) {
-            with(OpenAIEmbeddingsModel()) {
-                with(PineconeVectorStore(apiKey = pineconeKey, url = pineconeUrl, namespace = pineconeNamespace)) {
-                    SemanticSearch()
-                }
-            }
-        }
+        // TODO: Add eLLMental SemanticSearch component
     }
 
     suspend fun learn(input: LearnInput) {

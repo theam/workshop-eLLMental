20ea1fa6b23bfa85f1f7524c021d4306c496635b
Borja Zarco <borjazarco@theagilemonkeys.com>
2023-08-03 10:58:19 +0100

    Hotfix: Code fixes (#3)
    * rename project on settings.gradle
    
    * Add TODOs and throw UnsupportedOperationExceptions on methods to be implemented
    
    * Improve BookIngestionService

--- workshop-java/src/main/java/com/theagilemonkeys/workshop/services/SemanticSearchService.java
+++ workshop-java/src/main/java/com/theagilemonkeys/workshop/services/SemanticSearchService.java
@@ -1,41 +1,28 @@
 package com.theagilemonkeys.workshop.services;
 
-import com.aallam.openai.client.*;
-import com.theagilemonkeys.ellmental.embeddingsmodel.openai.OpenAIEmbeddingsModel;
-import com.theagilemonkeys.ellmental.semanticsearch.SearchInput;
 import com.theagilemonkeys.ellmental.semanticsearch.SearchOutput;
 import com.theagilemonkeys.ellmental.semanticsearch.SemanticSearch;
 import com.theagilemonkeys.workshop.config.SemanticSearchConfiguration;
-import com.theagilemonkeys.ellmental.vectorstore.pinecone.PineconeVectorStore;
 import kotlin.Unit;
 import org.springframework.stereotype.Service;
 
 import java.util.concurrent.CompletableFuture;
 
-import static com.theagilemonkeys.ellmental.embeddingsmodel.openai.OpenAIClientKt.OpenAIClient;
-
 @Service
 public class SemanticSearchService {
-    private final SemanticSearch semanticSearch;
+    private SemanticSearch semanticSearch;
 
     public SemanticSearchService(SemanticSearchConfiguration semanticSearchConfiguration) {
-        String openAIKey = semanticSearchConfiguration.getOpenaiKey();
-        String pineconeKey = semanticSearchConfiguration.getPineconeKey();
-        String pineconeUrl = semanticSearchConfiguration.getPineconeUrl();
-
-        OpenAI openAI = //TODO: Build an OpenAI client;
-        OpenAIEmbeddingsModel openAIEmbeddingsModel = //TODO: Create OpenAIEmbeddingsModel;
-
-        PineconeVectorStore pineconeVectorStore = //TODO: Create ChromaVectorStore or PineconeVectorStore;
-
-        semanticSearch = //TODO: Create SemanticSearch;
+        // TODO: Add eLLMental SemanticSearch component
     }
 
     public CompletableFuture<Unit> learn(String file_path) {
-        //TODO: Implement method
+        // TODO: Implement method
+        throw new UnsupportedOperationException("TODO");
     }
 
     public CompletableFuture<SearchOutput> search(String text) {
-        //TODO: Implement method
+        // TODO: Implement method
+        throw new UnsupportedOperationException("TODO");
     }
 }

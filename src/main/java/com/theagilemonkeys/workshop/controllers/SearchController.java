7647577141178243e557fb976b7cebba0c9554b9
Alejandro Miranda <alejandromiranda@theagilemonkeys.com>
2023-09-12 10:29:34 +0100

    Add Cross Origin Annotation

--- src/main/java/com/theagilemonkeys/workshop/controllers/SearchController.java
+++ src/main/java/com/theagilemonkeys/workshop/controllers/SearchController.java
@@ -2,6 +2,7 @@ package com.theagilemonkeys.workshop.controllers;
 
 import com.theagilemonkeys.ellmental.core.schema.Embedding;
 import com.theagilemonkeys.workshop.services.EmbeddingsSpaceService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -18,6 +19,7 @@ public class SearchController {
     }
 
     @GetMapping("/search")
+    @CrossOrigin
     @ResponseBody
     public List<Embedding> search(@RequestParam String query, @RequestParam(defaultValue = "10") int itemsLimit) {
         // TODO: implement the search functionality using the EmbeddingsSpaceService

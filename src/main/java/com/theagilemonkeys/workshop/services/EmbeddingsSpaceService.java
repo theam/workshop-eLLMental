c10197b6049a6f6dc0d741ca0fe4ddd4651c5fe5
Javier Toledo <javier@theagilemonkeys.com>
2023-09-14 15:32:42 +0100

    Updated the dpendencies in the project's `build.gradle` file to use the ones from Maven Central

--- src/main/java/com/theagilemonkeys/workshop/services/EmbeddingsSpaceService.java
+++ src/main/java/com/theagilemonkeys/workshop/services/EmbeddingsSpaceService.java
@@ -24,7 +24,6 @@ public class EmbeddingsSpaceService {
         throw new UnsupportedOperationException("TODO");
     }
 
-    // TODO: Score is not returned
     public List<Embedding> search(String text, int itemsLimit) {
         // TODO: Implement method
         throw new UnsupportedOperationException("TODO");
